d6d31217be3b994f6ff16026785840196e6748bf
Quinn Keaveney <quinn@liiift.studio>
2025-08-14 10:40:41 -0700

    Add main export file for NPM package

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+/**
+ * Sanity Delete Unused Assets - Main Export
+ * Asset cleanup utility for Sanity Studio
+ */
+
+export { default as DeleteUnusedAssets } from './DeleteUnusedAssets'
+export { default } from './DeleteUnusedAssets'
+
+// Export types if needed
+export type {
+  DeleteUnusedAssetsProps,
+  AssetScanResult,
+  CleanupResult
+} from './DeleteUnusedAssets'
