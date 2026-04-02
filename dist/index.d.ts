72c3b28a6872e4459e15c14789263733166f9b76
Quinn Keaveney <qkeave@gmail.com>
2026-04-02 01:22:38 -0700

    Remove node_modules and dist from git tracking, add .gitignore

--- dist/index.d.ts
+++
@@ -1,7 +0,0 @@
-/**
- * Sanity Delete Unused Assets - Main Export
- * Asset cleanup utility for Sanity Studio
- */
-export { default as DeleteUnusedAssets } from './DeleteUnusedAssets';
-export { default } from './DeleteUnusedAssets';
-export type { DeleteUnusedAssetsProps, AssetScanResult, CleanupResult } from './DeleteUnusedAssets';
